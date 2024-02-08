fecefc69a8a3a3f79551dfe655028aef026f4949
hana <zerda@korea.ac.kr>
2024-02-08 14:00:50 +0900

    푸터 업데이트

--- src/page/footer.js
+++ src/page/footer.js
@@ -3,16 +3,32 @@ import React from 'react'
 import styled from 'styled-components'
 
 const footer = () => {
+  const handleButtonClick = () => {
+    const result = window.confirm('알림창이 뜹니다! 예 또는 아니요를 선택하세요.'); // 윈도우 confirm 창을 띄우는 함수
+    if (result) {
+      alert('사용자가 "예"를 선택했습니다.');
+    } else {
+      alert('사용자가 "아니요"를 선택했습니다.');
+    }
+  }
+  
   return (
     <Wrap>
+      <div>?</div>
+      <div>
         ◀ | ▶
+      </div>
+      <button onClick={handleButtonClick}>
+        제출
+      </button>
     </Wrap>
   )
 }
 
 const Wrap = styled.footer`
-    display: flex;
-    flex-direction: column;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    flex-direction: row;
     justify-content: center;
     align-items: center;
     width: 100%;
